fb2ae2d65b44f833846558bcf668ca3faf07f994
OrenGitHub <tuna.is.good.for.you@gmail.com>
2024-05-06 09:25:41 +0300

    :checkered_flag: Finished ( #7 ) ghsa 97m3 :smiley:

--- utils.pl
+++ utils.pl
@@ -11,9 +11,16 @@ utils_concrete_user_input_might_reach_function_call(UserInput, Call) :-
     utils_dataflow_path(UserInput, Arg),
     kb_arg_for_call(Arg, Call).
 
-utils_user_input(UserInput) :-
-    utils_user_input_originated_from_express_post_request_params(UserInput).
-    /* here will be a long list of all web frameworks and more ... */
+utils_user_input(UserInput) :- utils_user_input_originated_from_express_post_request_params(UserInput).
+utils_user_input(UserInput) :- utils_user_input_originated_from_laravel_post_request_params(UserInput).
+% add more web frameworks here ...
+
+utils_user_input_originated_from_laravel_post_request_params(UserInput) :-
+    utils_laravel_post_handler(Call),
+    kb_arg_for_call(Callback, Call),
+    kb_callable(Callback),
+    kb_callable_has_param(Callback, UserInput),
+    kb_param_has_name(UserInput, 'request').
 
 utils_user_input_originated_from_express_post_request_params(UserInput) :-
     utils_express_post_handler(Call),
@@ -21,6 +28,10 @@ utils_user_input_originated_from_express_post_request_params(UserInput) :-
     kb_callable(Callback),
     kb_callable_has_param(Callback, UserInput),
     kb_param_has_name(UserInput, 'req').
+
+utils_laravel_post_handler(Call) :-
+    kb_call(Call),
+    kb_has_fqn(Call, 'composer.Illuminate.Support.Facades.Route.post').
 
 utils_express_post_handler(Call) :-
     kb_call(Call),
@@ -39,3 +50,11 @@ utils_dataflow_path(U, V) :-
     kb_dataflow_edge(Y,Z),
     kb_dataflow_edge(Z,T),
     kb_dataflow_edge(T,V).
+utils_dataflow_path(U, V) :-
+    kb_dataflow_edge(U,W),
+    kb_dataflow_edge(W,X),
+    kb_dataflow_edge(X,Y),
+    kb_dataflow_edge(Y,Z),
+    kb_dataflow_edge(Z,T),
+    kb_dataflow_edge(T,M),
+    kb_dataflow_edge(M,V).
