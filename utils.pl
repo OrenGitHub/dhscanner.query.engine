e0ed4161d0f7acdd5a86baa8c6bd678badb2b5a1
OrenGitHub <tuna.is.good.for.you@gmail.com>
2024-06-04 17:19:34 +0300

    cve 2023 45674 done :)

--- utils.pl
+++ utils.pl
@@ -1,3 +1,23 @@
+:- style_check(-singleton).
+
+user_input_might_reach_method(Fqn) :-
+    strrchr(Fqn, MethodName),
+    kb_has_fqn(Call, FqnTag),
+    kb_call(Call),
+    string_concat(_, MethodName, FqnTag).
+
+strrchr(String, Value) :-
+    sub_string(String, Before, Length, 0, Value),
+    sub_string(String, BeforeTag, LengthTag, 0, ValueTag),
+    string_concat('.', Value, ValueTag),
+    succ(Length, LengthTag),
+    succ(BeforeTag, Before),
+    sub_string(String, _, 1, _, '.'),
+    \+ sub_string(Value, _, 1, _, '.').
+
+utils_user_input_might_reach_callable_whose_fqn_is(Fqn) :-
+    utils_user_input_might_reach_function_whose_fqn_is(Fqn).   
+
 user_input_might_reach_function(Fqn) :-
     utils_user_input_might_reach_function_whose_fqn_is(Fqn).
 
@@ -14,6 +34,7 @@ utils_concrete_user_input_might_reach_function_call(UserInput, Call) :-
 utils_user_input(UserInput) :- utils_user_input_originated_from_npm_express_post_request_params(UserInput).
 utils_user_input(UserInput) :- utils_user_input_originated_from_composer_laravel_post_request_params(UserInput).
 utils_user_input(UserInput) :- utils_user_input_originated_from_pip_gradio_button_click_dispatch(UserInput).
+utils_user_input(UserInput) :- utils_user_input_originated_from_ruby_rails_post_request_params(UserInput).
 % add more web frameworks here ...
 
 utils_user_input_originated_from_composer_laravel_post_request_params(UserInput) :-
@@ -36,6 +57,19 @@ utils_user_input_originated_from_pip_gradio_button_click_dispatch(UserInput) :-
     kb_callable(Callback),
     kb_callable_has_param(Callback, UserInput).
 
+utils_user_input_originated_from_ruby_rails_post_request_params(UserInput) :-
+    utils_ruby_rails_class_controller(Class),
+    kb_method_of_class(_, Class), % <--- Method ...
+    %kb_variable_inside_method(UserInput, Method),
+    kb_has_fqn(UserInput, 'params').
+
+utils_ruby_rails_class_controller(Class) :-
+    kb_class_name(Class, Name),
+    utils_ends_with(Name, 'Controller').
+
+utils_ends_with(Haystack, Needle) :-
+    sub_atom(Haystack, _, _, _, Needle). 
+
 utils_composer_laravel_post_handler(Call) :-
     kb_call(Call),
     kb_has_fqn(Call, 'composer.Illuminate.Support.Facades.Route.post').
@@ -48,24 +82,46 @@ utils_pip_gradio_button_click(Call) :-
     kb_call(Call),
     kb_has_fqn(Call, 'gradio.Button.click').
 
+utils_dataflow_edge(U, V) :- kb_dataflow_edge(U, V).
+utils_dataflow_edge(Arg, Param) :-
+    kb_arg_for_call(Arg, Call),
+    kb_has_fqn(Call, Fqn),
+    kb_has_fqn(Callable, Fqn),
+    kb_callable_has_param(Callable, Param).
+
 % until the abstract interpretation is working ...
-utils_dataflow_path(U, V) :- kb_dataflow_edge(U,V).
-utils_dataflow_path(U, V) :- kb_dataflow_edge(U,W), kb_dataflow_edge(W,V).
-utils_dataflow_path(U, V) :- kb_dataflow_edge(U,W), kb_dataflow_edge(W,X), kb_dataflow_edge(X,V).
-utils_dataflow_path(U, V) :- kb_dataflow_edge(U,W), kb_dataflow_edge(W,X), kb_dataflow_edge(X,Y), kb_dataflow_edge(Y,V).
-utils_dataflow_path(U, V) :- kb_dataflow_edge(U,W), kb_dataflow_edge(W,X), kb_dataflow_edge(X,Y), kb_dataflow_edge(Y,Z), kb_dataflow_edge(Z,V).
 utils_dataflow_path(U, V) :-
-    kb_dataflow_edge(U,W),
-    kb_dataflow_edge(W,X),
-    kb_dataflow_edge(X,Y),
-    kb_dataflow_edge(Y,Z),
-    kb_dataflow_edge(Z,T),
-    kb_dataflow_edge(T,V).
+    utils_dataflow_edge(U,V).
 utils_dataflow_path(U, V) :-
-    kb_dataflow_edge(U,W),
-    kb_dataflow_edge(W,X),
-    kb_dataflow_edge(X,Y),
-    kb_dataflow_edge(Y,Z),
-    kb_dataflow_edge(Z,T),
-    kb_dataflow_edge(T,M),
-    kb_dataflow_edge(M,V).
+    utils_dataflow_edge(U,W),
+    utils_dataflow_edge(W,V).
+utils_dataflow_path(U, V) :-
+    utils_dataflow_edge(U,W),
+    utils_dataflow_edge(W,X),
+    utils_dataflow_edge(X,V).
+utils_dataflow_path(U, V) :-
+    utils_dataflow_edge(U,W),
+    utils_dataflow_edge(W,X),
+    utils_dataflow_edge(X,Y),
+    utils_dataflow_edge(Y,V).
+utils_dataflow_path(U, V) :-
+    utils_dataflow_edge(U,W),
+    utils_dataflow_edge(W,X),
+    utils_dataflow_edge(X,Y),
+    utils_dataflow_edge(Y,Z),
+    utils_dataflow_edge(Z,V).
+utils_dataflow_path(U, V) :-
+    utils_dataflow_edge(U,W),
+    utils_dataflow_edge(W,X),
+    utils_dataflow_edge(X,Y),
+    utils_dataflow_edge(Y,Z),
+    utils_dataflow_edge(Z,T),
+    utils_dataflow_edge(T,V).
+utils_dataflow_path(U, V) :-
+    utils_dataflow_edge(U,W),
+    utils_dataflow_edge(W,X),
+    utils_dataflow_edge(X,Y),
+    utils_dataflow_edge(Y,Z),
+    utils_dataflow_edge(Z,T),
+    utils_dataflow_edge(T,M),
+    utils_dataflow_edge(M,V).
