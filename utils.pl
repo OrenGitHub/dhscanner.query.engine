dc2a387b58cb3177c8a550c7af181a556a31ad45
OrenGitHub <tuna.is.good.for.you@gmail.com>
2024-08-28 16:39:27 +0300

    fix bugs in utils.pl

--- utils.pl
+++ utils.pl
@@ -1,52 +1,10 @@
 :- style_check(-singleton).
 
-user_input_might_reach_method(Fqn) :-
-    strrchr(Fqn, MethodName),
-    kb_has_fqn(Call, FqnTag),
-    kb_call(Call),
-    string_concat(_, MethodName, FqnTag).
-
-strrchr(String, Value) :-
-    sub_string(String, Before, Length, 0, Value),
-    sub_string(String, BeforeTag, LengthTag, 0, ValueTag),
-    string_concat('.', Value, ValueTag),
-    succ(Length, LengthTag),
-    succ(BeforeTag, Before),
-    sub_string(String, _, 1, _, '.'),
-    \+ sub_string(Value, _, 1, _, '.').
-
-utils_endswith(Haystack, Needle) :-
-    sub_string(Haystack, _, _, 0, Needle).
-
-user_input_might_reach_function_suffixed_by(Pqn) :-
-    utils_user_input_might_reach_function_whose_pqn_is(Pqn).
-
-utils_user_input_might_reach_function_whose_pqn_is(Pqn) :-
-    utils_user_input_might_reach_callable_whose_fqn_is(Fqn),
-    utils_endswith(Fqn, Pqn).
-
-utils_user_input_might_reach_callable_whose_fqn_is(Fqn) :-
-    utils_user_input_might_reach_function_whose_fqn_is(Fqn).   
-
 user_input_might_reach_function(Fqn) :-
-    utils_user_input_might_reach_function_whose_fqn_is(Fqn).
-
-utils_user_input_might_reach_function_whose_fqn_is(Fqn) :-
-    kb_has_fqn(Call, Fqn),
-    utils_concrete_user_input_might_reach_function_call(_, Call).
-
-utils_concrete_user_input_might_reach_function_call(UserInput, Call) :-
-    utils_user_input(UserInput),
     kb_call(Call),
+    kb_has_fqn(Call, Fqn),
     utils_dataflow_path(UserInput, Call),
-    kb_arg_for_call(Arg, Call).
-
-% This is for cases like Ruby's SomeClass.method.call
-% it is not labeled as a call, since at the syntactic level
-% it's just an expression ( fixed while working on CVE-2024-33667 )
-utils_concrete_user_input_might_reach_function_call(UserInput, Call) :-
-    utils_user_input(UserInput),
-    utils_dataflow_path(UserInput, Call).
+    utils_user_input(UserInput).
 
 utils_user_input(UserInput) :- utils_user_input_originated_from_npm_express_post_request_params(UserInput).
 utils_user_input(UserInput) :- utils_user_input_originated_from_composer_laravel_post_request_params(UserInput).
@@ -81,17 +39,14 @@ utils_user_input_originated_from_pip_gradio_button_click_dispatch(UserInput) :-
     kb_callable_has_param(Callback, UserInput).
 
 utils_user_input_originated_from_ruby_rails_post_request_params(UserInput) :-
+    kb_has_fqn(UserInput, 'params'),
     utils_ruby_rails_class_controller(Class),
-    kb_method_of_class(Method, Class),
-    kb_variable_inside_method(UserInput, Method),
-    kb_has_fqn(UserInput, 'params').
+    kb_var_in_method(UserInput, Method),
+    kb_method_of_class(Method, Class).
 
 utils_ruby_rails_class_controller(Class) :-
-    utils_subclass_of(Class, ApplicationController),
-    kb_class_name( ApplicationController, 'ApplicationController' ).
-
-utils_ends_with(Haystack, Needle) :-
-    sub_atom(Haystack, _, _, _, Needle). 
+    utils_subclass_of(Class, Super),
+    kb_class_name(Super, 'ApplicationController' ).
 
 utils_composer_laravel_post_handler(Call) :-
     kb_call(Call),
@@ -153,3 +108,22 @@ utils_dataflow_path(U, V) :-
     utils_dataflow_edge(Z,T),
     utils_dataflow_edge(T,M),
     utils_dataflow_edge(M,V).
+utils_dataflow_path(U, V) :-
+    utils_dataflow_edge(U,A),
+    utils_dataflow_edge(A,B),
+    utils_dataflow_edge(B,C),
+    utils_dataflow_edge(C,D),
+    utils_dataflow_edge(D,E),
+    utils_dataflow_edge(E,F),
+    utils_dataflow_edge(F,G),
+    utils_dataflow_edge(G,V).
+utils_dataflow_path(U, V) :-
+    utils_dataflow_edge(U,A),
+    utils_dataflow_edge(A,B),
+    utils_dataflow_edge(B,C),
+    utils_dataflow_edge(C,D),
+    utils_dataflow_edge(D,E),
+    utils_dataflow_edge(E,F),
+    utils_dataflow_edge(F,G),
+    utils_dataflow_edge(G,H),
+    utils_dataflow_edge(H,V).
