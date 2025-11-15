145702a8ab47b8229d0ab4d8dec1cbde6f0dc7bf
OrenGitHub <tuna.is.good.for.you@gmail.com>
2025-11-15 19:50:47 +0200

    cleanups :nail_care: ignore swipl warnings

--- template.pl
+++ template.pl
@@ -2,7 +2,15 @@
 
 :- discontiguous kb_class_name/2.
 :- discontiguous kb_subclass_of/2.
+:- discontiguous kb_const_string/2.
+:- discontiguous kb_param_has_type/2.
+:- discontiguous kb_param_has_name/2.
+:- discontiguous kb_arg_i_for_call/3.
+:- discontiguous kb_method_of_class/2.
+:- discontiguous kb_param_i_of_callable/3.
+:- discontiguous kb_class_has_named_super/2.
 :- discontiguous kb_callable_annotated_with/2.
+:- discontiguous kb_class_has_resolved_super/2.
 :- discontiguous kb_callable_annotated_with_user_input_inside_route/2.
 
 :- [ '{KNOWLEDGE_BASE}' ].
